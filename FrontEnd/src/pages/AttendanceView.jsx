314a1eb58d1bccbcc78cdcbd530ab960f2432d24
Karan <kc2532002@gmail.com>
2025-01-20 16:58:19 +0530

    ui

---
+++ FrontEnd/src/pages/AttendanceView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AttendanceView = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AttendanceView
--- FrontEnd/src/pages/LandingPage.jsx
+++ FrontEnd/src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/face-icon.gif";
-import logo1 from "../assets/image-p.png";
+import logo1 from "../assets/image.png";
 import { Link } from "react-router-dom";
 import Typewriter from 'typewriter-effect';
 
